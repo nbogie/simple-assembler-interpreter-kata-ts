1094263c08cc7dd3fb968d6c03aa04872daf9b0f
nbogie <neillbogie@googlemail.com>
2022-10-27 22:29:02 +0100

    doc expected interpreter output

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -149,7 +149,7 @@ function interpret(programInstructions: string[]): Registers {
     return registers;
 }
 
-// interpret(["mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a"]);
-interpret(["mov a -10", "mov b a", "inc a", "dec b", "jnz a -2"]);
+// interpret(["mov a -10", "mov b a", "inc a", "dec b", "jnz a -2"]);
+// should yield { a: 0, b: -20 }
 
 export { interpret, execute, Registers };
