8bb7ff5d0e0941817518391e3e225848df277dc5
nbogie <neillbogie@googlemail.com>
2022-10-29 12:17:13 +0100

    tidy tests

--- src/interpreter/interpreter.test.ts
+++ src/interpreter/interpreter.test.ts
@@ -1,13 +1,26 @@
 import { interpret, executeInstruction, Registers } from "./interpreter";
 
-test("interpret() works with original codewars tests", function () {
-    expect(
-        interpret(["mov a -10", "mov b a", "inc a", "dec b", "jnz a -2"])
-    ).toEqual({ a: 0, b: -20 });
+describe("interpret()", () => {
+    test("with original codewars tests", function () {
+        expect(
+            interpret(["mov a -10", "mov b a", "inc a", "dec b", "jnz a -2"])
+        ).toEqual({ a: 0, b: -20 });
 
-    expect(
-        interpret(["mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a"])
-    ).toEqual({ a: 1 });
+        expect(
+            interpret([
+                "mov a 5",
+                "inc a",
+                "dec a",
+                "dec a",
+                "jnz a -1",
+                "inc a",
+            ])
+        ).toEqual({ a: 1 });
+    });
+
+    test("with empty prog", function () {
+        expect(interpret([])).toEqual({});
+    });
 });
 
 describe("test executeInstruction", () => {
